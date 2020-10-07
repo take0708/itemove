6cbc0ed9704abb62f8a9d0b22ffd5bff327b4a56
take0708 <pk_er_stbo@yahoo.co.jp>
2020-10-07 21:21:09 +0900

    ルーティング、コントローラー追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,5 +22,4 @@ class ItemsController < ApplicationController
 
   def destroy
   end
-end
 end
