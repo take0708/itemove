16280cb23d9f4e915c106abeeadb324c8ecac5de
take0708 <pk_er_stbo@yahoo.co.jp>
2020-10-08 08:38:36 +0900

    itemマイグレーション

--- db/migrate/20201007122317_create_items.rb
+++ db/migrate/20201007122317_create_items.rb
@@ -1,7 +1,9 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.integer :code, null: false
+      t.string :name, null: false
+      t.integer :vol, null: false
       t.timestamps
     end
   end
