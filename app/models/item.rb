2d532730e9ce43416bea592e695fc187843e7759
take0708 <pk_er_stbo@yahoo.co.jp>
2020-12-08 20:17:53 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :code, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9999999 }
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :vol, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9999999 }
 end
